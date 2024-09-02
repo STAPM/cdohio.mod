7aa08949a91c0d5fbef128fcde390178d37bd226
Damon Morris <d.j.morris@sheffield.ac.uk>
2024-09-02 11:44:35 +0100

    Extract change in government tax receipts for tobacco and alcohol in the GenExpenditure function

--- R/GenExpenditure.R
+++ R/GenExpenditure.R
@@ -1,12 +1,14 @@
 #' Calculate Demand Changes
 #'
-#' Generate changes in demand for alcohol, tobacco, gambling, and food. Using the
+#' Generate changes in demand for alcohol, tobacco, gambling, and food using the
 #' saved expenditure data for the four product types in the model.
 #'
 #' Note that for alcohol and tobacco this function also calculates changes in demand in basic prices as well as
 #' in purchaser prices. As alcohol and tobacco are combined in the model, and have very different levels of taxation
 #' relative to retail price, the conversion from purchaser prices to basic prices is done here separately for
-#' alcohol and tobacco rather than within the model, as is the case for other products.
+#' alcohol and tobacco rather than within the model, as is the case for other products. Because of this, this function also
+#' calculates the change in tax less subsidies on products for alcohol and tobacco so that this is
+#' accounted for in the model.
 #'
 #' @param year Numeric integer. Year of expenditure data to use.
 #' @param change_food Numeric integer. Percentage change in expenditure on 19 categories of food;
@@ -89,20 +91,32 @@ GenExpenditure <- function(year,
 
   tobacco_data <- cdohio.mod::tobacco_expenditure[year == y,]
 
-  tobacco_data[, pct_change_licit := change_tobacco_licit]
-  tobacco_data[, pct_change_illicit := change_tobacco_illicit]
+  ## get percentage changes
+  tobacco_data[tobacco_category == "cigarettes", pct_change_licit := change_tobacco_licit[1] ]
+  tobacco_data[tobacco_category == "cigarettes", pct_change_illicit := change_tobacco_illicit[1] ]
+  tobacco_data[tobacco_category == "hrt", pct_change_licit := change_tobacco_licit[2] ]
+  tobacco_data[tobacco_category == "hrt", pct_change_illicit := change_tobacco_illicit[2] ]
 
+  ## calculate expenditure changes
   tobacco_data[, exp_change_licit := ((1 + pct_change_licit) * spend_licit_mn) - spend_licit_mn]
   tobacco_data[, exp_change_licit_bp := exp_change_licit * (1 - tax_pct - 0.1426)]
   tobacco_data[, exp_change_illicit := ((1 + pct_change_illicit) * spend_illicit_mn) - spend_illicit_mn]
 
+  ## calculate change in tax take
+  tobacco_data[, tax := exp_change_licit * tax_pct]
+
+  ## sum over all tobacco products
   tobacco_data <- tobacco_data[, .(exp_change_licit = sum(exp_change_licit),
                                    exp_change_licit_bp = sum(exp_change_licit_bp),
-                                   exp_change_illicit = sum(exp_change_illicit))]
+                                   exp_change_illicit = sum(exp_change_illicit),
+                                   tax_change = sum(tax))]
 
+  ## extract outputs
   tobacco_vec <- c(as.numeric(tobacco_data[,"exp_change_licit"]),
                    as.numeric(tobacco_data[,"exp_change_licit_bp"]),
                    as.numeric(tobacco_data[,"exp_change_illicit"]))
+
+  tobacco_tax <- as.numeric(tobacco_data[,"tax_change"])
 
   names(tobacco_vec) <- c("tobacco_l","tobacco_l_bp","tobacco_i")
 
@@ -117,16 +131,30 @@ GenExpenditure <- function(year,
 
   alcohol_data <- cdohio.mod::alcohol_expenditure[year == y,]
 
-  alcohol_data[, pct_change := change_alcohol]
+  ## get percentage changes
+  alcohol_data[alcohol_category == "beer",    pct_change := change_alcohol[1] ]
+  alcohol_data[alcohol_category == "cider",   pct_change := change_alcohol[2] ]
+  alcohol_data[alcohol_category == "wine",    pct_change := change_alcohol[3] ]
+  alcohol_data[alcohol_category == "spirits", pct_change := change_alcohol[4] ]
 
+  ## calculate expenditure changes
   alcohol_data[, exp_change_alcohol := ((1 + pct_change) * exp_alcohol_mn) - exp_alcohol_mn]
   alcohol_data[, exp_change_alcohol_bp := exp_change_alcohol * (1 - tax_pct - 0.1426 - 0.2515)]
 
-  alcohol_data <- alcohol_data[, .(exp_change_alcohol = sum(exp_change_alcohol),
-                                   exp_change_alcohol_bp = sum(exp_change_alcohol_bp))]
+  ## calculate tax changes
+  alcohol_data[, tax := exp_change_alcohol * tax_pct]
 
+  ## sum over all alcohol products
+
+  alcohol_data <- alcohol_data[, .(exp_change_alcohol = sum(exp_change_alcohol),
+                                   exp_change_alcohol_bp = sum(exp_change_alcohol_bp),
+                                   tax_change = sum(tax) )]
+
+  ## extract outputs
   alcohol_vec <- c(as.numeric(alcohol_data[,"exp_change_alcohol"]),
                    as.numeric(alcohol_data[,"exp_change_alcohol_bp"]))
+
+  alcohol_tax <- as.numeric(alcohol_data[,"tax_change"])
 
   names(alcohol_vec) <- c("alcohol","alcohol_bp")
 
@@ -136,6 +164,8 @@ GenExpenditure <- function(year,
   return(list(food = food_vec,
               gambling = gambling_vec,
               tobacco = tobacco_vec,
-              alcohol = alcohol_vec))
+              alcohol = alcohol_vec,
+              tob_tax = tobacco_tax,
+              alc_tax = alcohol_tax))
 
 }
