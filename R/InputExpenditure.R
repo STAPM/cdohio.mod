ffe4698de18edf6f7cfa570b78881bec6356b09c
Damon Morris <d.j.morris@sheffield.ac.uk>
2025-09-03 10:29:15 +0100

    adjust input expenditure function

--- R/InputExpenditure.R
+++ R/InputExpenditure.R
@@ -12,26 +12,7 @@
 #' calculates the change in tax less subsidies on products for alcohol and tobacco so that this is
 #' accounted for in the model.
 #'
-#' @param year Numeric integer. Year of expenditure data to use - default is 2019.
-#' @param change_food Numeric integer. Absolute change in expenditure (£mn) on 19 categories of food;
-#' 1. Milk, 2. Cheese, 3. Carcase meat, 4. Non-carcase meat and meat products, 5. Fish, 6. Eggs,
-#' 7. Fats, 8. Sugar and preserves, 9. Potatoes, 10. Fruit and veg, 11. Bread, 12. Flour, 13. Cakes, buns, and pastries,
-#' 14. Biscuits and crispbreads, 15. Cereals and cereal products, 16. Beverages, 17. Other food and drink, 18. Soft drinks,
-#' 19. Confectionary.
-#' @param change_gambling Numeric integer. Absolute change in expenditure (£mn) (gross gambling yield) on 9
-#' gambling categories (arcades, betting (non-remote), bingo (non-remote), casino (non-remote),
-#' betting (remote), bingo (remote), casino (remote), lotteries, the National Lottery)
-#' @param change_tobacco_licit Numeric vector. Absolute change in expenditure (£mn) on 2
-#' licit tobacco product categories (cigarettes, handrolled tobacco)
-#' @param change_tobacco_illicit Numeric vector. Absolute change in expenditure (£mn) on 2
-#' illicit tobacco product categories (cigarettes, handrolled tobacco)
-#' @param change_alcohol Numeric vector. Absolute change in expenditure (£mn) on 4 alcohol
-#' product categories (beer, cider, spirits, wine)
-#' @param reallocate_food Numeric. Reallocating changed expenditure on food to other food categories.
-#' Select one of the seven food categories in the input-output model to reallocate the changed spending
-#' on food to; (1) meat and meat products (2) fish, fruit, and vegetables (3) oils and fats (4) dairy
-#' products (5) grains and starch (6) bakery and farinaceous products (7) other food products. (Default value
-#' is `NULL` which means no reallocation across food categories).
+#' @param food_input_data Data table. Must contain the following columns - `CPA`, `Product`, and `exp_change`
 #'
 #'
 #' @return A list object
@@ -42,60 +23,29 @@
 #' \dontrun{
 #'
 #' }
-InputExpenditure <- function(year = 2019,
-                             change_food =            rep(0,19),
-                             change_gambling =        rep(0,9),
-                             change_tobacco_licit =   rep(0,2),
-                             change_tobacco_illicit = rep(0,2),
-                             change_alcohol =         rep(0,4),
-                             reallocate_food = NULL){
-
-  y <- copy(year)
+InputExpenditure <- function(food_input_data){
 
   ###################################
   #### Changes in food expenditure
 
-  food_data <- cdohio.mod::food_expenditure[year == y,]
+  food_data <- food_input_data[, .(exp_change = sum(exp_change)), by = c("CPA","Product")]
 
-  food_data[, exp_change := change_food]
-
-  food_data <- food_data[, .(exp_change = sum(exp_change)), by = "cpa"]
-
-  food_vec <- c(as.numeric(food_data[cpa == "meat"              ,"exp_change"]),
-                as.numeric(food_data[cpa == "fish_fruit_and_veg","exp_change"]),
-                as.numeric(food_data[cpa == "oils_and_fats"     ,"exp_change"]),
-                as.numeric(food_data[cpa == "dairy"             ,"exp_change"]),
-                as.numeric(food_data[cpa == "grains_and_starch" ,"exp_change"]),
-                as.numeric(food_data[cpa == "bakery"            ,"exp_change"]),
-                as.numeric(food_data[cpa == "other_food"        ,"exp_change"]))
+  food_vec <- c(as.numeric(food_data[Product == "Preserved meat and meat products"                                         ,"exp_change"]),
+                as.numeric(food_data[Product == "Processed and preserved fish, crustaceans, molluscs, fruit and vegetables","exp_change"]),
+                as.numeric(food_data[Product == "Vegetable and animal oils and fats"                                       ,"exp_change"]),
+                as.numeric(food_data[Product == "Dairy products"                                                           ,"exp_change"]),
+                as.numeric(food_data[Product == "Grain mill products, starches and starch products"                        ,"exp_change"]),
+                as.numeric(food_data[Product == "Bakery and farinaceous products"                                          ,"exp_change"]),
+                as.numeric(food_data[Product == "Other food products"                                                      ,"exp_change"]),
+                as.numeric(food_data[Product == "Soft drinks"                                                              ,"exp_change"]),
+                as.numeric(food_data[Product == "Food and beverage serving services"                                       ,"exp_change"]))
 
   names(food_vec) <- c("meat","fish_fruit_and_veg","oils_and_fats","dairy",
-                       "grains_and_starch","bakery","other_food")
-
-  ## reallocate change in spending on food to the selected category
-
-  if (!is.null(reallocate_food)){
-
-    j <- reallocate_food
-
-    food_exp_all_reallocate <- -1*sum(food_vec)
-
-    food_vec[j] <- food_vec[j] + food_exp_all_reallocate
-
-  }
+                       "grains_and_starch","bakery","other_food","soft_drinks","out_of_home")
 
   #########################################
   #### Changes in gambling expenditure
 
-  gambling_data <- cdohio.mod::gambling_expenditure[year == y,]
-
-  gambling_data[, exp_change := change_gambling]
-
-  gambling_data <- gambling_data[, .(exp_change = sum(exp_change))]
-
-  gambling_vec <- as.numeric(gambling_data[,"exp_change"])
-
-  names(gambling_vec) <- c("gambling")
 
   #########################################
   #### Changes in tobacco expenditure
@@ -106,36 +56,6 @@ InputExpenditure <- function(year = 2019,
   ## Assumes tobacco import % = joint tobacco and alcohol import % and distributors trading
   ## margin for tobacco = 0
 
-  tobacco_data <- cdohio.mod::tobacco_expenditure[year == y,]
-
-  ## get percentage changes
-  tobacco_data[tobacco_category == "cigarettes", change_licit := change_tobacco_licit[1] ]
-  tobacco_data[tobacco_category == "cigarettes", change_illicit := change_tobacco_illicit[1] ]
-  tobacco_data[tobacco_category == "hrt", change_licit := change_tobacco_licit[2] ]
-  tobacco_data[tobacco_category == "hrt", change_illicit := change_tobacco_illicit[2] ]
-
-  ## calculate expenditure changes
-  tobacco_data[, exp_change_licit := change_licit]
-  tobacco_data[, exp_change_licit_bp := exp_change_licit * (1 - tax_pct - 0.1426)]
-  tobacco_data[, exp_change_illicit := change_illicit]
-
-  ## calculate change in tax take
-  tobacco_data[, tax := exp_change_licit * tax_pct]
-
-  ## sum over all tobacco products
-  tobacco_data <- tobacco_data[, .(exp_change_licit = sum(exp_change_licit),
-                                   exp_change_licit_bp = sum(exp_change_licit_bp),
-                                   exp_change_illicit = sum(exp_change_illicit),
-                                   tax_change = sum(tax))]
-
-  ## extract outputs
-  tobacco_vec <- c(as.numeric(tobacco_data[,"exp_change_licit"]),
-                   as.numeric(tobacco_data[,"exp_change_licit_bp"]),
-                   as.numeric(tobacco_data[,"exp_change_illicit"]))
-
-  tobacco_tax <- as.numeric(tobacco_data[,"tax_change"])
-
-  names(tobacco_vec) <- c("tobacco_l","tobacco_l_bp","tobacco_i")
 
   #########################################
   #### Changes in alcohol expenditure
@@ -146,43 +66,16 @@ InputExpenditure <- function(year = 2019,
   ## Assumes alcohol import % = joint tobacco and alcohol import % and distributors trading
   ## margin for alcohol = joint tobacco and alcohol
 
-  alcohol_data <- cdohio.mod::alcohol_expenditure[year == y,]
 
-  ## get percentage changes
-  alcohol_data[alcohol_category == "beer",    change := change_alcohol[1] ]
-  alcohol_data[alcohol_category == "cider",   change := change_alcohol[2] ]
-  alcohol_data[alcohol_category == "wine",    change := change_alcohol[3] ]
-  alcohol_data[alcohol_category == "spirits", change := change_alcohol[4] ]
-
-  ## calculate expenditure changes
-  alcohol_data[, exp_change_alcohol := change]
-  alcohol_data[, exp_change_alcohol_bp := exp_change_alcohol * (1 - tax_pct - 0.1426 - 0.2515)]
-
-  ## calculate tax changes
-  alcohol_data[, tax := exp_change_alcohol * tax_pct]
-
-  ## sum over all alcohol products
-
-  alcohol_data <- alcohol_data[, .(exp_change_alcohol = sum(exp_change_alcohol),
-                                   exp_change_alcohol_bp = sum(exp_change_alcohol_bp),
-                                   tax_change = sum(tax) )]
-
-  ## extract outputs
-  alcohol_vec <- c(as.numeric(alcohol_data[,"exp_change_alcohol"]),
-                   as.numeric(alcohol_data[,"exp_change_alcohol_bp"]))
-
-  alcohol_tax <- as.numeric(alcohol_data[,"tax_change"])
-
-  names(alcohol_vec) <- c("alcohol","alcohol_bp")
 
   ###################################
   #### Return vectors as a list
 
   return(list(food = food_vec,
-              gambling = gambling_vec,
-              tobacco = tobacco_vec,
-              alcohol = alcohol_vec,
-              tob_tax = tobacco_tax,
-              alc_tax = alcohol_tax))
+              gambling = 0,
+              tobacco = rep(0,3),
+              alcohol = rep(0,2),
+              tob_tax = 0,
+              alc_tax = 0))
 
 }
